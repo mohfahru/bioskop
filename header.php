6dcb7997da15aadf6c8405f3a730b87bed3748ab
mohfahru <38067932+mohfahru@users.noreply.github.com>
2018-07-15 22:42:06 +0700

    Add files via upload
    Header

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Home - Home Page | Cinema - Free Website Template from Templates.com</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Place your description here" />
+<meta name="keywords" content="put, your, keyword, here" />
+<meta name="author" content="Templates.com - website templates provider" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Gill_Sans_400.font.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+<!--[if lt IE 7]>
+  <script type="text/javascript" src="js/ie_png.js"></script>
+  <script type="text/javascript">
+     ie_png.fix('.png, .link1 span, .link1');
+  </script>
+  <link href="ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+</head>
+<body id="page1">
+<div class="tail-top">
+  <div class="tail-bottom">
+    <div id="main">
+<!-- HEADER -->
+      <div id="header">
+        <div class="row-1">
+          <div class="fleft"><a href="index.html">Cinema <span>World</span></a></div>
+          <ul>
+            <li><a href="index.html"><img src="images/icon1-act.gif" alt="" /></a></li>
+            <li><a href="contact-us.html"><img src="images/icon2.gif" alt="" /></a></li>
+            <li><a href="sitemap.html"><img src="images/icon3.gif" alt="" /></a></li>
+          </ul>
+        </div>
+        <div class="row-2">
+          <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="about-us.html">Jadwal</a></li>
+            <li><a href="articles.html">Daftar Film</a></li>
+          </ul>
+        </div>
+      </div>
